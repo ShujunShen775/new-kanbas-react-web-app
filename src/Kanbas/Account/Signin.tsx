0744ab13611dc44a433c6405841d4cc1512f8638
shen.shuj <shen.shuj@northeastern.edu>
2024-08-10 21:05:42 +0800

    update

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -5,8 +5,8 @@ import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
 export default function Signin() {
   const [credentials, setCredentials] = useState<any>({
-    username: "n3",
-    password: "111",
+    username: "",
+    password: "",
   });
   const [error, setError] = useState("");
   const navigate = useNavigate();
